0e9fda400ad8c5dd57224e01c0931ad5e259514e
Andrea Lai <alai.arpas@gmail.com>
2025-10-08 08:03:33 +0200

    _build/dev/lib/elaixir/priv/data/my_sqlite.sqlite3

--- lib/elaixir/repo.ex
+++ lib/elaixir/repo.ex
@@ -6,6 +6,7 @@ defmodule Elaixir.Repo do
   @impl true
   def init(_type, config) do
     db_path = Path.join([:code.priv_dir(:elaixir), "data", "my_sqlite.sqlite3"])
+    IO.puts(db_path)
 
     {:ok, Keyword.put(config, :database, db_path)}
   end
